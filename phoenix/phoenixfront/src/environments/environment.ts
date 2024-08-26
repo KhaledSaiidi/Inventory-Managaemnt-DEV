a31e2986aeb801d4cf2aa96768503a8cbdbdd54a
KhaledSaiidi <Khaled.Saiidi@outlook.com>
2024-08-26 12:58:23 +0100

    working on prod similare env on docker v3

--- phoenix/phoenixfront/src/environments/environment.ts
+++ phoenix/phoenixfront/src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   keycloak: {
-    url: 'http://proxy:9002', // Use environment variable
+    url: 'http://localhost:8181', // Use environment variable
     realm:'phoenixstock',
     clientId:'front-client'
   },
-    url: '/api' // Use environment variable
+    url: 'http://localhost:9000' // Use environment variable
   //url: 'http://proxy:9002/api'
 };
